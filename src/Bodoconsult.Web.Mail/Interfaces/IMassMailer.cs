b82740dc5acba04ec27914fc5eadf4dbd87736d3
Robert Leisner <robert.leisner@bodoconsult.de>
2026-09-16 16:25:52 +0200

    More refactoring

--- src/Bodoconsult.Web.Mail/Interfaces/IMassMailer.cs
+++ src/Bodoconsult.Web.Mail/Interfaces/IMassMailer.cs
@@ -1,47 +1,47 @@
-// Copyright (c) Bodoconsult EDV-Dienstleistungen. All rights reserved.
+//// Copyright (c) Bodoconsult EDV-Dienstleistungen. All rights reserved.
 
-using System.Collections.Generic;
-using Bodoconsult.Web.Mail.Models;
+//using System.Collections.Generic;
+//using Bodoconsult.Web.Mail.Models;
 
-namespace Bodoconsult.Web.Mail.Interfaces;
+//namespace Bodoconsult.Web.Mail.Interfaces;
 
-/// <summary>
-/// Enhancement of <see cref="IMailer"/> for mass mailer implementations
-/// </summary>
-public interface IMassMailer : IMailer
-{
-    /// <summary>
-    /// HTML-Body für eMails
-    /// </summary>
-    string Body { get; set; }
+///// <summary>
+///// Enhancement of <see cref="IMailer"/> for mass mailer implementations
+///// </summary>
+//public interface IMassMailer : IMailer
+//{
+//    /// <summary>
+//    /// HTML-Body für eMails
+//    /// </summary>
+//    string Body { get; set; }
 
-    /// <summary>
-    /// Subject for the mail
-    /// </summary>
-    string Subject { get; set; }
+//    /// <summary>
+//    /// Subject for the mail
+//    /// </summary>
+//    string Subject { get; set; }
 
-    /// <summary>
-    /// Sending mail address
-    /// </summary>
-    string From { get; set; }
+//    /// <summary>
+//    /// Sending mail address
+//    /// </summary>
+//    string From { get; set; }
 
-    /// <summary>
-    /// Default salutation tu use: default is "Sehr geehrte Damen und Herren"
-    /// </summary>
-    string DefaultSalutation { get; set; }
+//    /// <summary>
+//    /// Default salutation tu use: default is "Sehr geehrte Damen und Herren"
+//    /// </summary>
+//    string DefaultSalutation { get; set; }
 
-    /// <summary>
-    /// All mail addresses the mail will be sent to
-    /// </summary>
-    IList<MailReceiver> To { get; set; }
+//    /// <summary>
+//    /// All mail addresses the mail will be sent to
+//    /// </summary>
+//    IList<MailReceiver> To { get; set; }
 
-    /// <summary>
-    /// Contains all found images in the document
-    /// </summary>
-    IList<ImageMetaData> Images { get; set; }
+//    /// <summary>
+//    /// Contains all found images in the document
+//    /// </summary>
+//    IList<ImageMetaData> Images { get; set; }
 
-    /// <summary>
-    /// Send mail to all mail addresses registered in <see cref="To"/>
-    /// </summary>
-    void SendMails();
-}
+//    /// <summary>
+//    /// Send mail to all mail addresses registered in <see cref="To"/>
+//    /// </summary>
+//    void SendMails();
+//}
